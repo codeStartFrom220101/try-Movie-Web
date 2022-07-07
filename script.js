7c8b34d5457565f61dfb0dbd4e1659365c563eae
SCY <shen421226@gmail.com>
2022-07-07 21:58:46 +0800

    get more search by similar list

--- script.js
+++ script.js
@@ -4,18 +4,25 @@ const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?api_key=d0971917ed8
 
 const movieList = document.querySelector('.movie-list');
 const search = document.querySelector('.search');
+const searchBtn = document.querySelector('.searchBtn');
 const searchTerm = document.querySelector('.searchTerm');
+const sidebarBtn = document.querySelector('.sidebarBtn');
+const sidebar = document.querySelector('.sidebar');
+const getMoreBtn = movieList.querySelector('.get-more-btn');
 const popupClose = document.querySelector('.popup-close');
 const popupContainer = document.querySelector('.popup-container');
+const popup = document.querySelector('.popup');
 const movieDetails = document.querySelector('.movie-details-container');
 const trailerBtn = document.querySelector('.trailer-btn');
 const trailerContainer = document.querySelector('.trailer-container');
+let sidebarMove = 'off';
+let searchBy = 'popular';
+let page = 1;
 
 async function getTMDBData(url) {
     const resp = await fetch(url)
     const respData = await resp.json();
     showMovies(respData.results);
-    return respData;
 }
 
 async function getTMDBDataById(url) {
@@ -24,27 +31,22 @@ async function getTMDBDataById(url) {
     return respData;
 }
 
-
-
-
-
-
 getTMDBData(TMBDUrl);
 
 function showMovies(movies) {
-    // clear list
-    movieList.innerHTML = '';
 
+    // clear list
     movies.forEach(movie => {
         const {
             poster_path,
             title,
             vote_average,
             id
-        } = movie
+        } = movie;
         const movieEl = document.createElement('li')
+        movieEl.dataset.id = id;
         movieEl.innerHTML = `
-                <img src="${IMGPATH + `${poster_path? poster_path : '/vQs0SFbMAZsvDF3aGtrVtDRmrl2.jpg'}`}" alt="${title}" data-id="${id}">
+                <img src="${IMGPATH + `${poster_path? poster_path : '/vQs0SFbMAZsvDF3aGtrVtDRmrl2.jpg'}`}" alt="${title}">
                 <div class="movie-info">
                     <h3>${title}</h3>
                     <span class="${getClassByRate(vote_average)}">${vote_average}</span>
@@ -52,8 +54,19 @@ function showMovies(movies) {
         `
         movieList.appendChild(movieEl);
     });
+    if (searchBy !== 'search') {
+        movieList.appendChild(getMoreBtn);
+    }
 }
 
+getMoreBtn.addEventListener('click', () => {
+    page++;
+    const url = `https://api.themoviedb.org/3/movie/${searchBy}?api_key=d0971917ed87f0e2070a34523ce6a2fc&language=en-US&page=${page}`
+    getTMDBData(url);
+})
+
+
+// rate point color
 function getClassByRate(point) {
     if (point >= 8) {
         return 'green'
@@ -64,9 +77,11 @@ function getClassByRate(point) {
     }
 }
 
-search.addEventListener('submit', searchMovies)
+search.addEventListener('submit', searchMovies);
 
 function searchMovies(e) {
+    searchBy = 'search';
+    movieList.innerHTML = '';
     e.preventDefault();
     const term = searchTerm.value;
     console.log(term);
@@ -86,22 +101,47 @@ function searchMovies(e) {
 //     img.style = `background-image:linear-gradient(to bottom, transparent, rgb(39, 37, 50)), url(${photo});`
 // })
 
-popupClose.addEventListener('click', () => {
-    popupContainer.classList.add('hidden');
+// sidebar slide 
+sidebarBtn.addEventListener('click', () => {
+    if (sidebarMove === 'off') {
+        sidebar.classList.add('active');
+        sidebarBtn.classList.add('active');
+        sidebarMove = 'on';
+    } else {
+        sidebar.classList.remove('active');
+        sidebarBtn.classList.remove('active');
+        sidebarMove = 'off'
+    }
 })
 
+// sidebar searchBy
+sidebar.addEventListener('click', async (e) => {
+    page = 1;
+    movieList.innerHTML = '';
+    searchBy = e.target.closest('li').dataset.search;
+    const url = `https://api.themoviedb.org/3/movie/${searchBy}?api_key=d0971917ed87f0e2070a34523ce6a2fc&language=en-US&page=1`
+    getTMDBData(url);
+})
+
+// click to close popup
+popupClose.addEventListener('click', () => {
+    popupContainer.classList.add('hidden');
+    movieDetails.innerHTML = '';
+})
+
+// get movie id by click
 movieList.addEventListener('click', (e) => {
     if (e.target.nodeName !== "IMG") {
         return;
     }
-    const id = e.target.closest('img').dataset.id
+    const id = e.target.closest('li').dataset.id
     showPopup(id);
 })
 
+// movie info
 async function showPopup(id) {
-    const SEARCHIDAPI = `https://api.themoviedb.org/3/movie/${id}?api_key=d0971917ed87f0e2070a34523ce6a2fc&append_to_response=credits`;
-    const movie = await getTMDBDataById(SEARCHIDAPI);
-    console.log(id);
+    const url = `https://api.themoviedb.org/3/movie/${id}?api_key=d0971917ed87f0e2070a34523ce6a2fc&append_to_response=credits`;
+    const movie = await getTMDBDataById(url);
     const {
         poster_path,
         backdrop_path,
@@ -112,8 +152,9 @@ async function showPopup(id) {
         vote_average,
         vote_count
     } = movie;
+
+    // render movie info
     popupContainer.classList.remove('hidden');
-    movieDetails.dataset.id = id;
     movieDetails.innerHTML = `
         <div class="img" style="background-image:linear-gradient(to bottom, rgba(0,0,0,0), #22254b), url('${IMGPATH + backdrop_path}');">
         </div>
@@ -150,18 +191,24 @@ async function showPopup(id) {
                         ${getCast(credits.cast)}
                     </ul>
                 </div>
+                <div class="similar">
+                    <h4>Similar !!!</h4>
+                    <ul class="similar-movie">
+                        ${await getSimilar(id)}
+                    </ul>
+                </div>
             </div>
         </div>
     `
 
+    // get trailer
     trailerBtn.addEventListener('click', async () => {
-        console.log(movie);
         const id = movie.id;
         const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=d0971917ed87f0e2070a34523ce6a2fc`;
         const allVideo = await getTMDBDataById(url);
         let trailer = allVideo.results.filter(video => video.type === "Trailer");
         trailer = trailer[0];
-        console.log(trailer);
+        popup.style.opacity = '.5'
         trailerContainer.classList.remove('hidden');
         trailerContainer.innerHTML = `
             <button class="trailer-close"><i class="fa-solid fa-xmark"></i></button>
@@ -173,15 +220,26 @@ async function showPopup(id) {
 
         const trailerClose = trailerContainer.querySelector('.trailer-close');
         trailerClose.addEventListener('click', () => {
+            popup.style.opacity = '1'
             trailerContainer.innerHTML = "";
             trailerContainer.classList.add('hidden');
         })
 
+    });
+
+    const similarMovie = movieDetails.querySelector('.similar-movie');
+    similarMovie.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        console.log(id);
+        if (id) {
+            showPopup(id);
+        }
     })
 }
 
-// ${genres.forEach(genre => `<span>${genre.name}</span>`)}
 
+
+// render genres
 function getGenre(genres) {
     let str = "";
     genres.forEach(genre => {
@@ -190,6 +248,7 @@ function getGenre(genres) {
     return str;
 }
 
+// render cast
 function getCast(cast) {
     let str = "";
     cast.forEach(actor => {
@@ -207,6 +266,38 @@ function getCast(cast) {
     return str
 }
 
+// guess what u like
+async function getSimilar(id) {
+    const url = `https://api.themoviedb.org/3/movie/${id}/similar?api_key=d0971917ed87f0e2070a34523ce6a2fc&language=en-US&page=1`
+    const respData = await getTMDBDataById(url);
+    console.log(respData);
+    const movies = respData.results;
+
+
+
+    let str = '';
+    movies.forEach((movie) => {
+        const {
+            poster_path,
+            title,
+            id,
+            vote_average
+        } = movie
+        str += `
+            <li>
+                <img src="${IMGPATH + `${poster_path? poster_path : '/vQs0SFbMAZsvDF3aGtrVtDRmrl2.jpg'}`}" alt="${title}" data-id="${id}">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                </div>
+            </li>
+    `
+    });
+
+    return str;
+}
+
+// star rated
 function getStar(point) {
     if (point == 10) {
         return "show_rated_5"
